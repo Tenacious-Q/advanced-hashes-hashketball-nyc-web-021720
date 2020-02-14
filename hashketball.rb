41cadb6aaeead83563fb9480b7ac96963e8540d5
Michael Quigley <mikequigley@quigleypublishing.com>
2020-02-14 05:29:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,17 @@
 # Write your code here!
 
+def game_hash
 
-
-
-
-
-
-
-
-
+hash = {
+  :home => {
+    :team_name => "Brooklyn Nets",
+    :colors => ["Black", "White"],
+    :players => [{}]
+  },
+  :away => {
+    :team_name => "Charlotte Hornets",
+    :colors => ["Turquoise", "Purple"],
+    :players => [{}]
+  }
+}
+end
