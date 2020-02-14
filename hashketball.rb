fc910b9700582f76759d103ac1577437a814835f
Michael Quigley <mikequigley@quigleypublishing.com>
2020-02-14 06:39:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -137,15 +137,15 @@ def num_points_scored(players_name)
 end
 
 def shoe_size(players_name)
-  game_hash.each do |place, team| 
-    team.each do |attribute, data| 
-      if attribute == :players 
-        data.each do |player| 
-          if player[:player_name]  == players_name 
+  game_hash.each do |place, team|
+    team.each do |attribute, data|
+      if attribute == :players
+        data.each do |player|
+          if player[:player_name]  == players_name
             return player[:points]
           end
-        end 
+        end
       end
     end
   end
-end 
+end
